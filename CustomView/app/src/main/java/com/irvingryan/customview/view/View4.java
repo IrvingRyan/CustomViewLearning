f3f0d2041f0470d4317a826d71475b26878469a5
IrvingRyan <20463220632@qq.com>
2016-08-15 10:06:25 +0800

    add own font

--- CustomView/app/src/main/java/com/irvingryan/customview/view/View4.java
+++ CustomView/app/src/main/java/com/irvingryan/customview/view/View4.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
@@ -15,6 +16,7 @@ import android.view.View;
  */
 public class View4 extends View {
 
+    private Typeface typeface;
     private Paint paint;
 
     public View4(Context context) {
@@ -60,6 +62,10 @@ public class View4 extends View {
         paint.setStrokeWidth(1);
         paint.setAntiAlias(true);
         paint.setTextSize(60);//设置文字大小
+        paint.setStyle(Paint.Style.STROKE);
+        //5.更改文字字体
+        typeface=Typeface.createFromAsset(getContext().getApplicationContext().getAssets(),"jian_luobo.ttf");
+        paint.setTypeface(typeface);
     }
 
     @Override
@@ -67,25 +73,32 @@ public class View4 extends View {
         super.onDraw(canvas);
         String text="都说钱是王八蛋，可长得真好看";
         //1.绘图样式区别
-//        paint.setStyle(Paint.Style.FILL);
-//        canvas.drawText(text,50,100,paint); //填充样式
-//        paint.setStyle(Paint.Style.STROKE);
-//        canvas.drawText(text,50,200,paint); //描边
-//        paint.setStyle(Paint.Style.FILL_AND_STROKE);
-//        canvas.drawText(text,50,300,paint); //填充且描边
-        //2.绘图样式及倾斜度
-        //样式设置
-        paint.setFakeBoldText(true);//设置是否为粗体文字
-        paint.setUnderlineText(true);//设置下划线
-        paint.setStrikeThruText(true);//设置带有删除线效果
         paint.setStyle(Paint.Style.FILL);
-        paint.setTextSkewX((float) -0.25);//设置字体水平倾斜度，普通斜体字是-0.25，可见往右斜
         canvas.drawText(text,50,100,paint); //填充样式
         paint.setStyle(Paint.Style.STROKE);
-        paint.setTextSkewX((float) 0.25);//水平倾斜度设置为：0.25，往左斜
         canvas.drawText(text,50,200,paint); //描边
         paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        paint.setTextScaleX(2);//设置拉伸效果
         canvas.drawText(text,50,300,paint); //填充且描边
+        //2.绘图样式及倾斜度
+        //样式设置
+//        paint.setFakeBoldText(true);//设置是否为粗体文字
+//        paint.setUnderlineText(true);//设置下划线
+//        paint.setStrikeThruText(true);//设置带有删除线效果
+//        paint.setStyle(Paint.Style.FILL);
+//        paint.setTextSkewX((float) -0.25);//设置字体水平倾斜度，普通斜体字是-0.25，可见往右斜
+//        canvas.drawText(text,50,100,paint); //填充样式
+//        paint.setStyle(Paint.Style.STROKE);
+//        paint.setTextSkewX((float) 0.25);//水平倾斜度设置为：0.25，往左斜
+//        canvas.drawText(text,50,200,paint); //描边
+//        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+//        paint.setTextScaleX(2);//设置拉伸效果
+//        canvas.drawText(text,50,300,paint); //填充且描边
+        //3.指定单个文字位置绘制
+//        canvas.drawPosText(text,new float[]{50,50,150,150,250,250,350,350},paint);//点的个数必须与字数相同
+        //4.按路径绘制文字(参照View3)
+//        Path path=new Path();
+//        path.addCircle(500,500,500, Path.Direction.CW);//顺时针
+//        canvas.drawPath(path,paint);
+//        canvas.drawTextOnPath(text,path,100,100,paint);
     }
 }
